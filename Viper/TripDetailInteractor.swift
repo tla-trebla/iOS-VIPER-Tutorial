cdc7c64a13b518830f86dfe0a90da70e76d01ad0
Albert Pangestu <stamina.left@gmail.com>
2022-01-10 02:18:23 +0700

    Expose tripName and tripNamePublisher when the trip.name's value changes.

--- Viper/TripDetailInteractor.swift
+++ Viper/TripDetailInteractor.swift
@@ -37,6 +37,9 @@ class TripDetailInteractor {
     
     private let cancellables = Set<AnyCancellable>()
     
+    var tripName: String { trip.name }
+    var tripNamePublisher: Published<String>.Publisher { trip.$name }
+    
     init(trip: Trip, model: DataModel, mapInfoProvider: MapDataProvider) {
         self.trip = trip
         self.mapInfoProvider = mapInfoProvider
