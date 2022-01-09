a43c006a79225c7f80c49112a3870a1a567ea804
Albert Pangestu <stamina.left@gmail.com>
2022-01-10 00:50:33 +0700

    Created property called 'trips' to collect results from the 'interactor'.

--- Viper/TripListPresenter.swift
+++ Viper/TripListPresenter.swift
@@ -32,6 +32,8 @@ import Combine
 class TripListPresenter: ObservableObject {
     private let interactor: TripListInteractor
     
+    @Published var trips: [Trip] = []
+    
     init(interactor: TripListInteractor) {
         self.interactor = interactor
     }
