624eacf15eab0cadda1bb2526184ea88079e8c47
Albert Pangestu <stamina.left@gmail.com>
2022-01-10 01:54:40 +0700

    Wrong annotation, it should be ObservedObject.

--- Viper/TripDetailView.swift
+++ Viper/TripDetailView.swift
@@ -30,7 +30,7 @@ import SwiftUI
 
 struct TripDetailView: View {
     
-    @ObservableObject var presenter: TripDetailPresenter
+    @ObservedObject var presenter: TripDetailPresenter
     
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
