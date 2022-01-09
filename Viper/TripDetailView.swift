46aa98ea1dead938c8877454cac77030cfd947f0
Albert Pangestu <stamina.left@gmail.com>
2022-01-10 01:53:50 +0700

    Add reference to the TripDetailPresenter property named presenter.

--- Viper/TripDetailView.swift
+++ Viper/TripDetailView.swift
@@ -29,6 +29,9 @@
 import SwiftUI
 
 struct TripDetailView: View {
+    
+    @ObservableObject var presenter: TripDetailPresenter
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
