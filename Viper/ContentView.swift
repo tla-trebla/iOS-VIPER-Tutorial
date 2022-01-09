fff8485c20b7c5d4fecc19a0284bfb23f0b548a6
Albert Pangestu <stamina.left@gmail.com>
2022-01-10 01:31:46 +0700

    Replace the VStack contents into a TripListView with a 'model' property.

--- Viper/ContentView.swift
+++ Viper/ContentView.swift
@@ -34,9 +34,7 @@ struct ContentView: View {
   var body: some View {
     NavigationView {
       VStack(spacing: 32) {
-        Text("Welcome to Visually Interesting Planned Easy Roadtrips (VIPER).")
-        Text("Just follow along in the tutorial.")
-        Text("Your trip awaits...")
+        TripListView(presenter: TripListPresenter(interactor: TripListInteractor(model: model)))
       }
     }
   }
