99ed97fa837e24efe2ca37ba6be46f8181f4554a
Albert Pangestu <stamina.left@gmail.com>
2022-01-10 11:54:08 +0700

    Replaced TripDetailView.body into TextField inside VStack that has navigationBarTitle and Save button in trailing.

--- Viper/TripDetailView.swift
+++ Viper/TripDetailView.swift
@@ -33,7 +33,13 @@ struct TripDetailView: View {
     @ObservedObject var presenter: TripDetailPresenter
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            TextField("Trip Name", text: presenter.setTripName)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding([.horizontal])
+        }
+        .navigationBarTitle(Text(presenter.tripName), displayMode: .inline)
+        .navigationBarItems(trailing: Button("Save", action: presenter.save))
     }
 }
 
